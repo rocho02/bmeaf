6aed270ea123f85d09d30239b298545214ce3fcb
ktzix <jozsef.schneider1996@gmail.com>
2018-11-16 19:47:37 +0100

    fix motor

--- bmeaf-device-webapp/routes/api.js
+++ bmeaf-device-webapp/routes/api.js
@@ -29,7 +29,7 @@ router.get('/start', function(req, res, next) {
      console.info('pulseWidth', pulseWidth, increment);
     if (pulseWidth >= 2000) {
        increment = -100;
-     } else if (pulseWidth <= 1000)
+     } else if (pulseWidth <= 1000){
          increment = 100;
      }
     res.json({message: 'started'});
