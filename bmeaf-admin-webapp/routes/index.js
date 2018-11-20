50fe746d63d0a6d451c8ef9d1620fd1970c5d2c9
ktzix <jozsef.schneider1996@gmail.com>
2018-11-20 13:10:11 +0100

    playing with pulsewidth

--- bmeaf-admin-webapp/routes/index.js
+++ bmeaf-admin-webapp/routes/index.js
@@ -5,7 +5,7 @@ let mqttClient = require('../mqtt');
 /* GET home page. */
 // noinspection JSUnresolvedFunction
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Motor Indítás' , layout: 'layout2.hbs' });
+  res.render('index', { title: 'Válassz adagot' , layout: 'layout2.hbs' });
 });
 
 module.exports = router;
